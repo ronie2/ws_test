a80d48bf98fc249fa16bcb8b1bfb41f0f15a21be
root <roman.nzn@gmail.com>
2016-11-02 15:16:43 +0000

    Basic server and clients websockets added

---
+++ server.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+import random
+
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print("< {}".format(name))
+
+    greeting = "Hello {}!".format(name)
+    await websocket.send(greeting)
+    print("> {}".format(greeting))
+
+port = random.randrange(1000, 10000)
+start_server = websockets.serve(hello, 'localhost', port)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+print('Port: ', port)
+asyncio.get_event_loop().run_forever()
+
